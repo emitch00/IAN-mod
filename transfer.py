9fed59ac40016ce1ea51913f4a27461f8f4245b7
emitch00 <70246125+emitch00@users.noreply.github.com>
2021-02-21 13:30:31 -0800

    Update transfer.py

--- transfer.py
+++ transfer.py
@@ -11,9 +11,7 @@ file = open("/content/groundtruth.txt", "r")
 count = {}
 
 for line in file:
-  commonWordsFile = open("/content/commonWords.txt", "r")
-  english = True
-  unique = True
+  aspect_term_list = open("/content/sortedGroundtruth.txt", "r")
   word_token = re.split(" |\t", line)
   fact = word_token[len(word_token) - 1]
   fact = fact.rstrip("\n")
@@ -28,37 +26,26 @@ for line in file:
       i = i.lower()
       # filter out empty strings, numbers, http links, @handles, and 'rt'
       if len(i) > 0 and (not (i.isdigit())) and i.find('http') == -1 and i[0] != "@" and i != 'rt':
-        # filter out words with non english letters
-        for letter in i:
-          if ord(letter) < 97 or ord(letter) > 122:
-            english = False
-            break
-        # only add to map if word has english letters
-        if (english):
-          # check that word isnt a common one
-          commonWordsFile = open("/content/commonWords.txt", "r")
-          for word in commonWordsFile:
+          # check if word is an aspect term
+          for word in aspect_term_list:
             word = word.rstrip("\n")
             if word == i:
-              unique = False
-              break
-
-          if unique:
-            i = i + ", " + fact
-            if i not in count: 
-              count[i] = 0 
-            count[i]+=1
+              if i not in count: 
+                count[i] = 0 
+              count[i]+=1
+              
+                i = 'aspect_term'
             
 file.close()
 commonWordsFile.close()
 
-sorted_list = sorted(count, reverse = True, key = lambda x: count[x])
-sorted_count = {}
-for i in sorted_list:
-  sorted_count[i] = count.get(i)
+maplist = {}
+for i in maplist:
+  maplist[i] = count.get(i)
+  print(maplist)
 
-new_file = open("/content/sortedGroundtruth.txt", "w")
-for key, value in sorted_count.items():
-  new_file.write(key+" : "+str(value)+"\n")
+#new_file = open("/content/data_sort.txt", "w")
+#for key, value in sorted_count.items():
+  #new_file.write(line +" : "+str(value)+"\n")
 
-new_file.close()
+#new_file.close()
