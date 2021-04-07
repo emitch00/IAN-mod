abb4e862988eb34d8181c537ebc77675bd6c60bc
emitch00 <70246125+emitch00@users.noreply.github.com>
2021-04-07 16:50:25 -0700

    Update main-pytorch.py

--- main-pytorch.py
+++ main-pytorch.py
@@ -30,61 +30,62 @@ embedding_file_name = 'data/glove.840B.300d.txt'
 
 def run(model, train_data, test_data):
   print('Training ...')
-    max_acc, max_f1, step = 0., 0., -1
+  max_acc, max_f1, step = 0., 0., -1
 
-    train_data_size = len(train_data[0])
-    train_data = F.Tensor.narrow(train_data) #check on whether .Dataset matters
-    train_data = train_data.shuffle(buffer_size=train_data_size).batch(batch_size, drop_remainder=True)
+  train_data_size = len(train_data[0])
+  train_data = F.Tensor.narrow(train_data) #check on whether .Dataset matters
+  train_data = train_data.shuffle(buffer_size=train_data_size).batch(batch_size, drop_remainder=True)
     
-    test_data_size = len(test_data[0])
-    test_data = F.Tensor.narrow(test_data) #check on whether .Dataset matters
-    test_data = test_data.batch(batch_size, drop_remainder=True)#check whether we need shuffle or not
+  test_data_size = len(test_data[0])
+  test_data = F.Tensor.narrow(test_data) #check on whether .Dataset matters
+  test_data = test_data.batch(batch_size, drop_remainder=True)#check whether we need shuffle or not
     
+  iterator = torchtext.data.iterator(train_data.output_types, train_data.output_shapes)
+  optimizer = optim.Adam(lr=learning_rate)
+  #write to log directory
+    
+  for i in range(n_epoch):
+    cost, predict_list, labels_list = 0., [], []
+    #not sure about this
     iterator = torchtext.data.iterator(train_data.output_types, train_data.output_shapes)
-    optimizer = optim.Adam(lr=learning_rate)
-    #write to log directory
+    for _ in range(math.floor(train_data_size / batch_size)):
+      data = iterator.get_next()
+      #replacing tape
+      predict, labels = model(data, dropout = 0.5)
+      loss_t = F.nll_loss(F.LogSoftmax(predict), labels)
+      loss = F.mean(loss_t)
+      cost += F.sum(loss_t)
+        
+      optimizer.zero_grad()
+      loss.backward()
+      optimizer.step()
+      #torch.autograd()
+        
+      predict_list.extend(F.argmax(F.softmax(predict)).numpy())
+      labels_list.extend(F.argmax(labels).numpy())
+        
+        
+    train_acc, train_f1, _, _ = evaluate(pred=predict_list, gold=labels_list)
+    train_loss = cost / train_data_size
+    #write to summary
+      
+    cost, predict_list, labels_list = 0., [], []
+    iterator = torchtext.data.iterator(test_data.output_types, test_data.output_shapes)
+    for _ in range(math.floor(test_data_size / batch_size)):
+      data = iterator.get_next()
+      predict, labels = model(data, dropout=1.0)
+      #torch.nn.functional.cross_entropy
+      loss_t = F.nll_loss(F.LogSoftmax(predict), labels)
     
-    for i in range(n_epoch):
-      cost, predict_list, labels_list = 0., [], []
-      #not sure about this
-      iterator = torchtext.data.iterator(train_data.output_types, train_data.output_shapes)
-      for _ in range(math.floor(train_data_size / batch_size)):
-        data = iterator.get_next()
-        #replacing tape
-        predict, labels = model(data, dropout = 0.5)
-        loss_t = F.nll_loss(F.LogSoftmax(predict), labels)
-        loss = F.mean(loss_t)
-        cost += F.sum(loss_t)
-        
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        #torch.autograd()
-        
-        predict_list.extend(F.argmax(F.softmax(predict)).numpy())
-        labels_list.extend(F.argmax(labels).numpy())
-        
-        
-      train_acc, train_f1, _, _ = evaluate(pred=predict_list, gold=labels_list)
-      train_loss = cost / train_data_size
-      #write to summary
+    test_acc, test_f1, _, _ = evaluate(pred=predict_list, gold=labels_list)
+    test_loss = cost/test_data_size
+    #write to summary
       
-      cost, predict_list, labels_list = 0., [], []
-      iterator = torchtext.data.iterator(test_data.output_types, test_data.output_shapes)
-      for _ in range(math.floor(test_data_size / batch_size)):
-            data = iterator.get_next()
-            predict, labels = model(data, dropout=1.0)
-            #torch.nn.functional.cross_entropy
-            loss_t = F.nll_loss(F.LogSoftmax(predict), labels)
-      test_acc, test_f1, _, _ = evaluate(pred=predict_list, gold=labels_list)
-      test_loss = cost/test_data_size
-      #write to summary
-      
-      if test_acc + test_f1 > max_acc + max_f1:
-            max_acc = test_acc
-            max_f1 = test_f1
-            step = i
-            #write to saver
+    if test_acc + test_f1 > max_acc + max_f1:
+      max_acc = test_acc
+      max_f1 = test_f1
+      step = i
+      #write to saver
             
       #print to console
   #print to console
